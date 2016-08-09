9f7c845c49cd629da5ebf1663c2e325bffe5e7e5
shuzheng <469741414@qq.com>
2016-08-09 18:09:20 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,53 +1,56 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+// 控制应用生命周期的模块。
+const {app} = electron;
+// 创建原生浏览器窗口的模块。
+const {BrowserWindow} = electron;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// 保持一个对于 window 对象的全局引用，如果你不这样做，
+// 当 JavaScript 对象被垃圾回收， window 会被自动地关闭。
+let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+	// 创建浏览器窗口。
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: false
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/main.html`)
+	// 加载应用的 index.html。
+	mainWindow.loadURL(`file://${__dirname}/main.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+	// 启用开发工具。
+	mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	// 当 window 被关闭，这个事件会被触发。
+	mainWindow.on('closed', () => {
+		// 取消引用 window 对象，如果你的应用支持多窗口的话，
+		// 通常会把多个 window 对象存放在一个数组里面，
+		// 与此同时，你应该删除相应的元素。
+		mainWindow = null;
+	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// Electron 会在初始化后并准备创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+	// 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+	// 否则绝大部分应用及其菜单栏会保持激活。
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('activate', () => {
+	// 在 macOS 上，当点击 dock 图标并且该应用没有打开的窗口时，
+	// 绝大部分应用会重新创建一个窗口。
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// 在这文件，你可以续写应用剩下主进程代码。
+// 也可以拆分成几个文件，然后用 require 导入。
